2ec31046c5e1fd6496c9e373cbf70b413b40be03
naoto7010code <n.m.1210@au.com>
2022-10-09 23:49:42 +0900

    ユーザー編集機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get 'messages/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "messages#index"
+  resources :users, only: [:edit, :update]
 end
